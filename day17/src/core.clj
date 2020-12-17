54808f7ee47f4d38917c5c9f0e84b6b01df2adf6
Naveen Yellapu <naveen.yellapu@cat.com>
2020-12-17 17:02:32 +0530

    Day 17 - Refactor

--- day17/src/core.clj
+++ day17/src/core.clj
@@ -4,16 +4,12 @@
             [clojure.spec.alpha :as s]))
 
 (defn parse [input]
-  (let [xs (reduce (fn [{:keys [i l] :as m} v]
-                     (if (= \. v)
-                       (update m :i inc)
-                       (-> (update m :points conj {:x (quot i l)
-                                                   :y (rem i l)
-                                                   :z 0
-                                                   :w 0})
-                           (update :i inc))))
-                   {:points #{} :i 0 :l (.length (first input))} (str/join input))]
-    (:points xs)))
+  (let [column-size (.length (first input))
+        s ^String (str/join input)]
+    (into #{} (for [i (range (.length s)) :let [v (.charAt s i)] :when (not= \. v)] {:x (quot i column-size)
+                                                                                     :y (rem i column-size)
+                                                                                     :z 0
+                                                                                     :w 0}))))
 
 (defn neighbours [{:keys [x y z w]}]
   (for [a [x (inc x) (dec x)]
@@ -26,24 +22,20 @@
     {:x a :y b :z c :w d}))
 
 (defn space [points]
-  (reduce #(union (into %1 (neighbours %2))) (into #{} points) points))
+  (reduce #(into %1 (neighbours %2)) (into #{} points) points))
 
 (defn cycle [points]
-  (loop [points points counter 0]
-    (if (= 6 counter)
-      points
-      (let [space (space points)
-            new-points (into #{} (for [p space :let [ns (neighbours p)
-                                                     c (count (filter #(contains? points %) ns))]
-                                       :when (or (and (contains? points p) (<= 2 c 3))
-                                                 (and (nil? (get p points)) (= c 3)))]
-                                   p))]
-        (recur new-points (inc counter))))))
+  (let [space (space points)]
+    (into #{} (for [p space :let [ns (neighbours p)
+                                  c (count (filter #(contains? points %) ns))]
+                    :when (or (and (contains? points p) (<= 2 c 3))
+                              (and (nil? (get p points)) (= c 3)))]
+                p))))
 
 (defn begin [input]
-  (let [active-points (-> input
-                          parse)]
-    (println (count (cycle active-points)))))
+  (println (count (nth (->> input
+                            parse
+                            (iterate cycle)) 6))))
 
 (defn -main [& args]
   (println "hello" args)
